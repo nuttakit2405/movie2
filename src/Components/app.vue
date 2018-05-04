4c535f83e8dcf305be35e4d5853305e3641c0a5a
kanyaruk <ahmkanyaruk@hotmail.com>
2018-05-04 17:46:23 +0700

    connect firebase

--- src/Components/app.vue
+++ src/Components/app.vue
@@ -1,14 +1,48 @@
 <template>
 <div class="box">
-    <h3 class="title">App</h3>
-    <movie></movie>
+    <h3 class="title">App: {{ movieId }}</h3>
+    <movie @chooseMovie="handleChooseMovie" :movieId="movieId"></movie>
 </div>  
 </template>
 
 <script>
+import firebase from 'firebase'
+
 import Movie from 'Components/movie.vue'
+import Seat from 'Components/seat.vue'
+
+const config = {
+    databaseURL: "https://movie-firebase.firebaseio.com/"
+}
+
+firebase.initializeApp(config)
+
+const db = firebase.database()
+
+const dbRef = db.ref('/').child('civilwar')
+dbRef.set('yyy')
 
 export default {
-    component: {Movie}
+    components: {Movie},
+    data(){
+        return {
+            movieId: '',
+            selectSeats: [],
+            status: { count: 0, price: 0}
+        }
+    },
+    methods: {
+        handleChooseMovie(movieId){
+            if (this.status.count){
+                if (confrim('Data will be lost ?')){
+                    this.status = {count: 0, price: 0}
+                    this.selectSeats = []
+                }else {
+                    return false
+                }
+            }
+            this.movieId = movieId
+        }
+    }
 }
 </script>
--- src/Components/movie.vue
+++ src/Components/movie.vue
@@ -1,5 +1,56 @@
 <template>
 <div class="box">
-    <h3 class="title">movie</h3>
+    <h3 class="title">{{ movieId }}</h3>
+    <div class="columns">
+        <div :key='m.id' v-for =" m in movies " :class= "className(m.id)" @click="chooseMovie(m.id)">
+            <figure class="image">
+                <!-- </img src="/movies/civilwar.jpg"> -->
+                <img :src="imgSrc(m.id)">
+                
+            </figure>
+        </div>    
+    </div>    
 </div>  
 </template>
+
+ <script>
+ import { movies } from 'other/movie.json'
+
+// console.log(movies)
+
+export default {
+    props: ['movieId'],
+    data(){
+        return{
+            movies
+        }
+    },
+    methods: {
+        imgSrc(movieId) {
+            return `/movies/${ movieId }.jpg`
+        },
+        chooseMovie(movieId){
+            // console.log('OK')
+            this.$emit('chooseMovie',movieId)
+        },
+        className(movieId){
+            return [
+                'column', 'pointer',
+                { 'chosen': this.movieId === movieId}
+            ]
+        }
+    },
+    mounted() {
+        this.chooseMovie(movies[0].id)
+    }
+}
+ </script>
+
+<style>
+.pointer{
+    cursor: pointer;
+}
+.chosen {
+    border-style:solid;
+}
+</style>
